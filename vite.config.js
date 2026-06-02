e66678643e6f01b8d067edfa488adfba2c34421d
Woodna Adrien <wadrien.dev@gmail.com>
2026-06-02 10:44:23 -0400

    Update portfolio deployment configuration

--- vite.config.js
+++ vite.config.js
@@ -3,13 +3,5 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
-
   base: "/responsive-portfolio/",
-
-  server: {
-    proxy: {
-      "/contact": "http://localhost:5000",
-      "/health": "http://localhost:5000",
-    },
-  },
 });
